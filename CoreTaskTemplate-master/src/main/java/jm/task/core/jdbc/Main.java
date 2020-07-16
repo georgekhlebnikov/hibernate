191af7698e1e50e44bc8f2574657f5013320a984
georgekhlebnikov <george.khlebnikov@yandex.ru>
2020-07-17 00:25:12 +0300

    commit and push message

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         List<User> users;
         UserService userServiceImpl = new UserServiceImpl();
