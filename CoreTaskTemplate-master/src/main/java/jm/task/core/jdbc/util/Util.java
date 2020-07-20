8d583e22840987881c4a442b5ce6371158a70425
georgekhlebnikov <george.khlebnikov@yandex.ru>
2020-07-20 14:31:15 +0300

    1.1.4 No cfg.XML - Hibernate config in Util.java

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,32 @@
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.cfg.Configuration;
+
 package jm.task.core.jdbc.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
+import java.util.Properties;
 import java.sql.*;
 
 public class Util {
-    //JDBC configuration
+    // For all:
     private static final String DB_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL =
-            "jdbc:mysql://localhost:3306/jmdao?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/" +
+            "jmdao?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false";
     private static final String DB_LOGIN = "root";
     private static final String DB_PASSWORD = "1234";
+    // Specific:
+    private static final String DB_DIALECT = "org.hibernate.dialect.MySQLDialect";
+    private static final String DB_SHOW_SQL = "true";
+    //private static final String CONTEXT_CLASS = "thread";
+    private static final String HBM2DDL_AUTO = "create-drop"; //create
 
+    //JDBC configuration:
     public Connection getConnection() {
         Connection connection = null;
 
@@ -27,13 +40,37 @@ public class Util {
         return connection;
     }
 
-    //Hibernate configuration
+    //Hibernate configuration:
     private static SessionFactory sessionFactory;
+
     static {
-        Configuration cfg = new Configuration().configure();
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                .applySettings(cfg.getProperties());
-        sessionFactory = cfg.buildSessionFactory(builder.build());
+        Configuration configuration = new Configuration();
+
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, DB_JDBC_DRIVER);
+        settings.put(Environment.URL, DB_URL);
+        settings.put(Environment.USER, DB_LOGIN);
+        settings.put(Environment.PASS, DB_PASSWORD);
+        settings.put(Environment.DIALECT, DB_DIALECT);
+        settings.put(Environment.SHOW_SQL, DB_SHOW_SQL);
+        //settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, CONTEXT_CLASS);
+        settings.put(Environment.HBM2DDL_AUTO, HBM2DDL_AUTO);
+
+        configuration
+                .setProperties(settings)
+                .addAnnotatedClass(jm.task.core.jdbc.model.User.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build();
+
+        sessionFactory = configuration
+                .buildSessionFactory(serviceRegistry);
+
+        //Configuration configuration = new Configuration().configure();
+        //StandardServiceRegistryBuilder registryBuilder =
+        //        new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        //sessionFactory = configuration.buildSessionFactory(registryBuilder.build());
     }
     public static SessionFactory getSessionfactory() {
         return sessionFactory;
